dbc31ccd22bbdeb5bb17c6182066d75d171493d3
volodya <perl_ovsky@mail.ru>
2015-01-13 01:02:52 +0200

    package com.javarush.test.level08.lesson11.bonus01;

--- src/Solution.java
+++ src/Solution.java
@@ -1,36 +1,44 @@
+import jdk.nashorn.internal.runtime.regexp.RegExp;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
-/**
- * Created by vvp on 30.12.2014.
- */
+/* Работа с датой
+1. Реализовать метод isDateOdd(String date) так, чтобы он возвращал true,
+ если количество дней с начала года - нечетное число,
+ иначе false
+2. String date передается в формате MAY 1 2013
+Пример:
+JANUARY 1 2000 = true
+JANUARY 2 2020 = false
+*/
+
 public class Solution {
 
+    public static void main(String[] args) throws IOException
+    {
+        //add your code here - напиши код тут
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String line = reader.readLine();
+        line = line.substring(0,1).toUpperCase() + line.substring(1).toLowerCase();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MMMM", Locale.ENGLISH);
+        Date month = null;
 
-        public static void main(String[] args) throws IOException
+        try
         {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-            String s = reader.readLine();
-
-            //Напишите тут ваш код
-
-            StringBuilder res = new StringBuilder(s.length());
-            String[] elements = s.split("[\\s\\xA0]+");
-
-
-
-            for (int i = 0; i < elements.length; i++)
-            {
-                if (i > 0){
-                    res.append(" ");
-                    res.append(Character.toUpperCase(elements[i].charAt(0))).append(
-                            elements[i].substring(1));
-                }
-                System.out.println(res);
-            }
+            month = simpleDateFormat.parse(line);
+        }
+        catch (ParseException e)
+        {
+            e.printStackTrace();
         }
 
- }
+        System.out.println("«" + line + " is " + (month.getMonth() + 1) + " month».");
+    }
 
+}
