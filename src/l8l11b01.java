00888a5ebf031ec903f6489805276c73f26149e4
vperlovskiy <perl_ovsky@mail.ru>
2015-01-14 20:14:37 +0200

    Fist

---
+++ src/l8l11b01.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by vvp on 14.01.2015.
+ */
+public class l8l11b01 {
+
+    public static void main(String[] args) throws IOException
+    {
+
+        Map<String, Integer> map = new HashMap<String, Integer>();
+    map.put("January", 1);
+    map.put("February", 2);
+    map.put("March", 3);
+    map.put("April", 4);
+    map.put("May", 5);
+    map.put("June", 6);
+    map.put("July", 7);
+    map.put("August", 8);
+    map.put("September", 9);
+    map.put("October", 10);
+    map.put("November", 11);
+    map.put("December", 12);
+
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    String m = reader.readLine();
+
+    for (Map.Entry<String, Integer> pair : map.entrySet())
+    {
+        String key = pair.getKey();
+        Integer value = pair.getValue();
+        if (pair.getKey().equals(m))
+            System.out.println(key + " is " + value + " month");
+    }
+  }
+}
