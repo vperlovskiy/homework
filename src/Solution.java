8950661544bae88400ad11e33da02c6c58fc5b85
vperlovskiy <perl_ovsky@mail.ru>
2015-01-29 18:00:32 +0200

    package com.javarush.test.level08.lesson11.bonus03;

--- src/Solution.java
+++ src/Solution.java
@@ -3,51 +3,44 @@ import jdk.nashorn.internal.runtime.regexp.RegExp;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.Locale;
+import java.util.*;
+/* Нужно добавить в программу новую функциональность
+Задача: Программа определяет, какая семья (фамилию) живёт в доме с указанным номером.
+Новая задача: Программа должна работать не с номерами домов, а с городами:
+Пример ввода:
+Москва
+Ивановы
+Киев
+Петровы
+Лондон
+Абрамовичи
 
-/* Работа с датой
-1. Реализовать метод isDateOdd(String date) так, чтобы он возвращал true,
- если количество дней с начала года - нечетное число,
- иначе false
-2. String date передается в формате MAY 1 2013
-Пример:
-JANUARY 1 2000 = true
-JANUARY 2 2020 = false
+Лондон
+
+Пример вывода:
+Абрамовичи
 */
 
 public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        //add your code here - напиши код тут
-        public static void main(String[] args) throws Exception
-        {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-            String[] array = new String[20];
-            for (int i = 0; i < array.length; i++)
-            {
-                array[i] = reader.readLine();
-            }
-
-            sort(array);
-
-            for (String x : array)
-            {
-                System.out.println(x);
+        //list of addresses
+        Map<String, String> location = new HashMap<String, String>();
+        while (true) {
+            String family = reader.readLine();
+            if (family.isEmpty()) break;
+            else {
+                String city = reader.readLine();
+                location.put(city, family);
             }
         }
+            //read home number
+            String town = reader.readLine();
 
-    public static void sort(String[] array)
-    {
-        //Напишите тут ваш код
-        Arrays.sort(array);
-    }
+            for (Map.Entry<String, String> entry : location.entrySet()){
+                if (entry.getKey().equals(town)) System.out.println(entry.getValue());
+                }
+        }
 
-    //Метод для сравнения строк: 'а' больше чем 'b'
-    public static boolean isGreaterThen(String a, String b)
-    {
-        return a.compareTo(b) > 0;
-    }
 }
